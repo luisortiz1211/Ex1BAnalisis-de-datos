ede658a4022f4c93abc74b3588781f5039fde86e
luisortiz1211 <66312625+luisortiz1211@users.noreply.github.com>
2021-01-18 23:31:54 -0500

    Cambio dirección atlas

--- Examen1B_AnalisisDeDatos/9_10.py
+++ Examen1B_AnalisisDeDatos/9_10.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pymongo 
 
 
-client = pymongo.MongoClient('mongodb://localhost:27017')
+client = pymongo.MongoClient('mongodb+srv://esfot:esfot@cluster0.p8olh.mongodb.net/couch_mongo_dakar2021?retryWrites=true&w=majority')
 db = client.get_database('couch_mongo_dakar2021')
 collection = db.get_collection('dakar2021')
 df = pd.DataFrame(list(collection.find()))
